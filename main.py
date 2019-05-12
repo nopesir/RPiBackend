7688ba7529204898f3588f984d4a058c12f13fdd
Luigi Ferrettino <s254300@studenti.polito.it>
2019-05-12 17:33:53 +0200

    Added control with RasbBLEConnector

--- main.py
+++ main.py
@@ -1,32 +1,44 @@
 from flask import Flask
+from flask import json
+from flask import request
 import subprocess
 import re
-from flask import json
+import gatt
+import time
+import socket
+from ESP32BLE import ESP32BLE
+from ESP32BLE import ESP32BLEManager
 
 app = Flask(__name__)
 
-def countoverlappingdistinct(pattern, thestring):
-  total = 0
-  start = 0
-  there = re.compile(pattern)
-  while True:
-    mo = there.search(thestring, start)
-    if mo is None: return total
-    total += 1
-    start = 1 + mo.start()
-
-@app.route("/getNConnected")
-def hello():
-
-    a = subprocess.run(["sh", "script.sh"], stdout=subprocess.PIPE)
-    b = countoverlappingdistinct("([0-9a-fA-F]:?){12}", a.stdout.decode("utf-8"))
-    c = '{"n": ' + str(b) + '}'
-
-    response = app.response_class(
-        response=json.dumps(c),
-        status=200,
-        mimetype='application/json'
-    )
+@app.route("/connect", methods=['GET'])
+def connect():
+    ssid = request.args.get('ssid')
+    passwd = request.args.get('passwd')
     
-    return response
+    manager = ESP32BLEManager(adapter_name='hci0')
+    manager.start_discovery()
+    manager.run()
+    manager.stop_discovery()
+    macs = manager.hashmac
     
+    if(not macs):
+      return "no devices"
+    
+    ip_address = ([l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2]
+                                if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)),
+                                                                      s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET,
+                                                                                                                             socket.SOCK_DGRAM)]][0][1]]) if l][0][0])
+    
+    for key, value in macs.items():
+        manager = gatt.DeviceManager(adapter_name='hci0')
+        device = ESP32BLE(ssid=ssid, password=passwd, manager=manager,
+                          mac_address=key, name=value, ip_address=ip_address)
+        device.connect()
+        manager.run()
+        print("Rebooting %s" % value + '..')
+        device.disconnect()
+        print("------------------------------")
+    
+    n_of_devices = len(macs)
+    names = macs.values()
