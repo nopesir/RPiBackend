722911eeccec650860d33e97bf308b063faafe91
Luigi Ferrettino <s254300@studenti.polito.it>
2019-07-04 17:43:18 +0200

    adde CURRENT_TIMESTAMP

--- main.py
+++ main.py
@@ -315,7 +315,7 @@ def on_message(mqtt_client, obj, msg):
         print(int((msg.payload).decode('utf-8')))
         conn = sqlite3.connect('/home/pi/local.db')
         c = conn.cursor()
-        c.execute("""INSERT INTO desired(id, timestamp, value) VALUES((?), (?), (?))""", str(msg.topic[:15]), int(time.mktime(datetime.datetime.today().timetuple())),  int((msg.payload).decode('utf-8')))
+        c.execute("""INSERT INTO desired(id, timestamp, value) VALUES((?), CURRENT_TIMESTAMP, (?))""", str(msg.topic[:15]), int((msg.payload).decode('utf-8')))
         conn.commit()
     else:
         esps[str(msg.topic[:15])] = json.loads(msg.payload)
