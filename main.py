26f045ad4d381789a49fea70afdb07081a32604f
Luigi Ferrettino <s254300@studenti.polito.it>
2019-07-04 20:39:08 +0200

    fix

--- main.py
+++ main.py
@@ -325,6 +325,7 @@ def on_message(mqtt_client, obj, msg):
 
     else:
         esps[str(msg.topic[:15])] = json.loads(msg.payload)
+        print("mannaia")
         state = jsonify(esps[str(msg.topic[:15])])
         print(state)
         conn = sqlite3.connect('/home/pi/local.db')
