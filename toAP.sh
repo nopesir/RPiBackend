5e346d2de4f69e4104fad23be6fdda15ab095e7c
Luigi Ferrettino <s254300@studenti.polito.it>
2019-07-07 09:52:39 +0200

    fixx

--- toAP.sh
+++ toAP.sh
@@ -2,14 +2,14 @@
 
 # ENABLE wlan0 IN dhcpcd.conf
 echo "Stoping $netInterface"
-ifconfig wlan0 down
+sudo ifconfig wlan0 down
 echo "Stoping wpa_supplicant"
-killall wpa_supplicant
+sudo killall wpa_supplicant
 echo "Attempting to start hostapd"
-/etc/init.d/hostapd stop
-/etc/init.d/hostapd start
+sudo /etc/init.d/hostapd stop
+sudo /etc/init.d/hostapd start
 echo "Setting IP Address for $netInterface"
-/sbin/ifconfig $netInterface wlan0 down
-/sbin/ifconfig $netInterface wlan0 up
+sudo /sbin/ifconfig $netInterface wlan0 down
+sudo /sbin/ifconfig $netInterface wlan0 up
 echo "Attempting to start dnsmasq"
-/etc/init.d/dnsmasq restart
+sudo /etc/init.d/dnsmasq restart
