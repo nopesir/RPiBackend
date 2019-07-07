66e09be9802b12938e3fee0823b832bb1450ad2b
Luigi Ferrettino <s254300@studenti.polito.it>
2019-07-07 09:36:14 +0200

    fix on restart

--- toAP.sh
+++ toAP.sh
@@ -6,8 +6,10 @@ ifconfig wlan0 down
 echo "Stoping wpa_supplicant"
 killall wpa_supplicant
 echo "Attempting to start hostapd"
+/etc/init.d/hostapd stop
 /etc/init.d/hostapd start
 echo "Setting IP Address for $netInterface"
 /sbin/ifconfig $netInterface wlan0 up
 echo "Attempting to start dnsmasq"
+/etc/init.d/dnsmasq stop
 /etc/init.d/dnsmasq start
