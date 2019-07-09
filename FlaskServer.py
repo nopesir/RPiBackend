18c0b8596c822617fc3e0992766473b15bc36bd4
Luigi Ferrettino <s254300@studenti.polito.it>
2019-07-09 15:52:56 +0200

    fixed upload config

--- FlaskServer.py
+++ FlaskServer.py
@@ -597,7 +597,7 @@ def get_mqtt():
 def upload_config(config):
     global apsta
     data = {
-        'device mac': "D4:25:8B:D9:E7:2F", 
+        'device_mac': "D4:25:8B:D9:E7:2F", 
         'nickname': "nopesir",
         'configuration': {}
     }
@@ -614,16 +614,15 @@ def upload_config(config):
     
     API_TOKEN = json.loads(response.text)['access_token']
 
-    print(API_TOKEN)
 
     data['configuration'] = config
 
-    print(json.dumps(data))
 
     response = requests.post(API_URI+"user/PL19-18/devices", data=json.dumps(data), headers={"Authorization": "JWT " + API_TOKEN, 'Content-Type': 'application/json'})
 
 def download_config():
     API_URI = "http://ec2-34-220-162-82.us-west-2.compute.amazonaws.com:5002/"
+
     response = requests.post(API_URI+"auth", data=json.dumps({'username':'PL19-18', 'password':'raspbian'}), headers={'Content-Type': 'application/json'})
     
     if not json.loads(response.text)['access_token']:
